359fd58367551985d604faca5950e3bfaed884cc
Anushil Raman <anushilraman1993@gmail.com>
2024-01-11 02:44:51 +0530

    Use of Link and Nav Link of  React Router instead of anchor tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <Link to="/"> {new Date().getFullYear()} React Learning</Link>
+    </footer>
   )
 }
